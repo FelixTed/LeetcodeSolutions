26bd0f2790260f91bb2f27c2c1d063d9bd37eb68
FelixTed <97131287+FelixTed@users.noreply.github.com>
2024-03-12 13:20:39 -0400

    Time: 1 ms (84.86%), Space: 41.4 MB (61.14%) - LeetHub

--- 2027-minimum-moves-to-convert-string/2027-minimum-moves-to-convert-string.java
+++ 2027-minimum-moves-to-convert-string/2027-minimum-moves-to-convert-string.java
@@ -1,15 +1,9 @@
 class Solution {
     public int minimumMoves(String s) {
-        StringBuilder stringBuilder = new StringBuilder(s);
-
-        int count = 0;
-
-        //NOTE: this does not convert the string correctly, but 
-        // works great for our purpose of getting the minimum number of moves possible
-        //With minimum runtime
+         int count = 0;
+        
         for(int i = 0; i< s.length();i++){
             if (s.charAt(i) == 'X'){
-                stringBuilder.replace(i,i+3,"OOO");
                 i+=2;
                 count++;
             }
